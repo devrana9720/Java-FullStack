9ea64fd4db1e9d323b4278d9e4815f81f554cc35
Dev Rana <devrana3256@gmail.com>
2026-08-08 14:49:22 +0530

    Changed Succesfull

---
+++ DSA/Serching/Binary_search.java
@@ -0,0 +1,28 @@
+public class Binary_search {
+    public static void main(String[] args) {
+
+        //Time Complexity: O(log n)
+        //Space Complexity: O(1)
+
+        int arr[]={10,20,30,40,50};
+        int tar=40;
+        int low=0;
+        int high=arr.length-1;
+        int steps=0;
+        while(low<=high){
+            int mid=(low+high)/2;
+            steps++;
+            if(arr[mid]==tar){
+                System.out.println("Element found at index: "+mid);
+                System.out.println("Number of steps: "+steps);
+                break;
+            }
+            else if(arr[mid]<tar){ 
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+    }
+}
