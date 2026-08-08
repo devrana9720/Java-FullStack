330e3a7dafd1f228771a92aeddcbc6fe85dc828b
Dev Rana <devrana3256@gmail.com>
2026-08-08 14:07:55 +0530

    Linear Search

---
+++ DSA/Linear_search.java
@@ -0,0 +1,13 @@
+
+public class Linear_search {
+    public static void main(String[] args) {
+        int arr[]={10,20,30,40,50};
+        int tar=40;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==tar){
+                System.out.println("Element found at index: "+i);
+                break;
+            }
+        }
+}
+}
